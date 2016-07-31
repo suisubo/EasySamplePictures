04bfa1d858abfa0590b00053d15cc8bb566bb4eb
Roger <subo.sui@gmail.com>
2016-07-31 17:32:12 -0400

    Major Change

---
+++ PrestaShop/config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'bB5YdHLXx7b4SVcDERqUB9ZDtsf9LO6fR0eLyw3ZzomJKParfTmQwDqW');
+define('_COOKIE_IV_', 'Rdppwmvf');
+define('_PS_CREATION_DATE_', '2016-07-29');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.6');
+define('_RIJNDAEL_KEY_', 'fwfErGwAUBuCLg6hmc6aNBJqkVCdqh88');
+define('_RIJNDAEL_IV_', 'tnCPUo3FB1MIHJUZ7VvDTA==');
