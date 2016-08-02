61a51d7855745d47b67410a565ad4f7e4f886fed
Roger <subo.sui@gmail.com>
2016-08-02 14:55:29 -0400

    small change another

--- PrestaShop/init.php
+++ PrestaShop/init.php
@@ -21,7 +21,7 @@
 *  @author PrestaShop SA <contact@prestashop.com>
 *  @copyright  2007-2016 PrestaShop SA
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-*  International Registered Trademark & Property of PrestaShop CA
+*  International Registered Trademark & Property of PrestaShop SA
 */
 
 if (isset(Context::getContext()->controller)) {
