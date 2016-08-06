fe49ed0715478c1975c414f67b17684b3c584d07
Roger <subo.sui@gmail.com>
2016-08-06 18:00:19 -0400

    service classes

---
+++ PrestaShop/classes/service/ServiceType.php
@@ -0,0 +1,20 @@
+<?php
+
+class ServicesCore extends ObjectModel
+{
+	public $id_vendor;
+	public $price;
+	public $active;
+	public $id_image;
+	
+	public static $definition = array(
+			'table' => 'service_type',
+			'primary' => 'id_service_type',
+			'fields' => array(
+					'price' =>        array('type' => self::TYPE_FLOAT, 'validate' => 'isPrice', 'required' => true),
+					'active' =>        array('type' => self::TYPE_BOOL, 'validate' => 'isBool', 'required' => true),
+					'id_image' =>                    array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true),					
+			),
+	);
+	
+}
