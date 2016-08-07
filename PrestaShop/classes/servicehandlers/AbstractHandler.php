44b2d0dd0227c840f432fc13aa6e61598b18b954
Roger <subo.sui@gmail.com>
2016-08-06 22:47:28 -0400

    service handler

--- PrestaShop/classes/servicehandlers/AbstractHandler.php
+++ PrestaShop/classes/servicehandlers/AbstractHandler.php
@@ -2,8 +2,16 @@
 
 abstract class AbstractHandlerCore
 {
-	abstract public function getRequiredInputs();
-	abstract public function processInputs($inputs);
+	const UI_REQUEST_CONFIRMATION = 1;
+	const UI_REQUEST_TEXT_INPUT = 2;
+	const UI_REQUEST_FILE_UPLOAD = 3;
+	const UI_REQUEST_FILE_DOWNLOAD = 4;
+	
+	abstract public function getRequiredUIInputs();
+	abstract public function getDisplayMessage();
+	abstract public function processUIInputs($inputs, $context, $mapping_ruls);
 	abstract public function getReadableStatusString($lang);
-	abstract public function getOutputTypes();
+	abstract public function getPossibleExitCodes();
+	abstract public function getOutputVariableNames();
+	abstract public function getInputVariableNames();
 }
