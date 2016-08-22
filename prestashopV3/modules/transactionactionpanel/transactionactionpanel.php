c4ccd5bae8c0153dc0c42f949ee127fdaf8e48a3
Roger <subo.sui@gmail.com>
2016-08-21 23:28:28 -0400

    services

--- prestashopV3/modules/transactionactionpanel/transactionactionpanel.php
+++ prestashopV3/modules/transactionactionpanel/transactionactionpanel.php
@@ -38,4 +38,65 @@ class transactionactionpanel extends Module
 			return false;
 			return true;
 	}
+	
+	public function hookdisplayOrderDetail($params)
+	{
+		$db = Db::getInstance();
+		
+		$order = $params["order"];
+		$id_order = $order->id;
+		
+		$sql = 'select * from '._DB_PREFIX_.'z_transaction where id_order = '.$id_order;
+		
+		$transactions = $db->ExecuteS($sql);
+		
+		if(!$transactions)
+			return '';
+		
+		foreach ($transactions as $transaction)		
+		{
+			$id_product = $transaction["id_product"];
+			$sql = 'select * from '._DB_PREFIX_.'z_service_product where id_product = '.$id_product;
+			
+			$service_products = $db->ExecuteS($sql);
+			
+			if(!$service_products)
+				continue;
+			
+			foreach ($service_products as $servie_product)
+			{
+				$sql = 'select * from '._DB_PREFIX_.'z_service_step where id_service_type = '
+						.$servie_product['id_service_type'].' AND id_step = '.$transaction['current_step'];
+							
+				$service_steps = $db->ExecuteS($sql);
+				
+				if(!$service_steps)
+					continue;
+				
+				foreach ($service_steps as $service_step)
+				{
+					$id_step_type = $service_step["id_step_type"];
+					$action_partner = $service_step["action_partner"];
+					$instruction = $service_step["instruction"];
+					$description = $service_step["description"];
+		
+					if($action_partner == 0)
+					{
+						$sql = 'select * from '._DB_PREFIX_.'z_step_type_ui where id_step_type = '.$id_step_type;
+							
+						$step_ui = $db->ExecuteS($sql);
+						
+						$transactions_ui[] = array("id_transaction" => $transaction['id_transaction'],
+								"instruction" => $instruction,
+								"description" => $description,
+								"ui_list" => $step_ui);
+					}
+		
+				}
+			}			
+		}
+		
+		
+		return true;
+	}
 }	
